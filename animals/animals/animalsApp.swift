7a0107f15e8f3bf51b15230329352185d316603e
skostiuk <>
2022-08-04 17:57:53 +0300

    create new proj

---
+++ animals/animals/animalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  animalsApp.swift
+//  animals
+//
+//  Created by skostiuk on 04.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct animalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
